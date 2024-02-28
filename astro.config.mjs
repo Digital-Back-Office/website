c8bdfd004f4c8e0b6c9570bac92ee68832a09202
Aditya Rana <ranaaditya01@outlook.com>
2024-02-28 20:07:21 +0530

    Added video layout 2 for check

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-    buildOptions: {
-        baseUrl: '/src/'
-    }
+  buildOptions: {
+    baseUrl: '/src/'
+  },
+  integrations: [mdx()]
 });
