e2f78da5a1e56d8db972a17cffe833b3f2bebcab
Aditya Rana <ranaaditya01@outlook.com>
2024-03-19 22:42:47 +0530

    Fixed bg video

--- src/scripts/google-analytics.js
+++ src/scripts/google-analytics.js
@@ -1,3 +1,5 @@
+const tag_id = import.meta.env.PUBLIC_GOOGLE_TAG_ID;
+
 (function (w, d, s, l, i) {
     w[l] = w[l] || []; w[l].push({
         'gtm.start':
@@ -5,4 +7,4 @@
     }); var f = d.getElementsByTagName(s)[0],
         j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
             'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
-})(window, document, 'script', 'dataLayer', 'GTM-MLJ9M4LT');
+})(window, document, 'script', 'dataLayer', tag_id);
