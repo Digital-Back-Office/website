81ae2374e5fe837daa99104d54b2f7c92d682b6d
Aditya Rana <ranaaditya01@outlook.com>
2024-03-26 13:58:53 +0530

    Resolved uk time issue

--- src/utils/helper/date-time.js
+++ src/utils/helper/date-time.js
@@ -1,26 +1,35 @@
+import convert from "uk-time";
 export default function dateTimeForCalendar(startDateTime) {
     // const TIMEOFFSET = '+05:30'
     let date = new Date(Date.parse(startDateTime));
+    const utcTime = date.toUTCString();
+    //console.log(utcTime);
+    const ukTime = convert.toUKTime(utcTime);
+    //console.log(ukTime);
+    const date1 = new Date(ukTime)
+    //console.log(date1);
     //console.log("parsed value:", date);
-    const year = date.getUTCFullYear();
-    const month = String(date.getUTCMonth() + 1).padStart(2, '0');
-    const day = String(date.getUTCDate()).padStart(2, '0');
-    const hour = String(date.getUTCHours()).padStart(2, '0');
-    const minute = String(date.getUTCMinutes()).padStart(2, '0');
+    const year = date1.getUTCFullYear();
+    const month = String(date1.getUTCMonth() + 1).padStart(2, '0');
+    const day = String(date1.getUTCDate()).padStart(2, '0');
+    const hour = String(date1.getUTCHours()).padStart(2, '0');
+    const minute = String(date1.getUTCMinutes()).padStart(2, '0');
 
     const start = `${year}-${month}-${day}T${hour}:${minute}:00`;
 
-    const endDate = new Date(date.getTime() + 30 * 60000);
+    const endDate = new Date(date1.getTime() + 30 * 60000);
     const endYear = endDate.getUTCFullYear();
     const endMonth = String(endDate.getUTCMonth() + 1).padStart(2, '0');
     const endDay = String(endDate.getUTCDate()).padStart(2, '0');
     const endHour = String(endDate.getUTCHours()).padStart(2, '0');
     const endMinute = String(endDate.getUTCMinutes()).padStart(2, '0');
 
+    const end = `${endYear}-${endMonth}-${endDay}T${endHour}:${endMinute}:00`;
+
     //console.log("start", start);
     //console.log("end", end);
 
-    const end = `${endYear}-${endMonth}-${endDay}T${endHour}:${endMinute}:00`;
+    
 
     return {
         start: start,
