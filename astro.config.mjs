f5c911b781c0ec767dc34d4ea0a055228d444de7
GOKULNATH RS <gokulnathpeace@gmail.com>
2026-02-20 19:16:58 +0530

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.digitalbackoffice.co.uk',
+  site: 'https://digitalbackoffice.co.uk',
   buildOptions: {
     baseUrl: '/src/'
   },
