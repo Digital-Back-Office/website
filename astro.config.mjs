d053576d5c3860b85ca5a2e9012560d249c2bc73
GOKULNATH-RS <gokulnathpeace@gmail.com>
2026-06-19 15:13:26 +0530

    fix: config fix for build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,11 @@
 import { defineConfig } from 'astro/config';
 
 import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://digitalbackoffice.co.uk',
-  buildOptions: {
-    baseUrl: '/src/'
-  },
   integrations: [
-    mdx(),
-    sitemap({
-      filter: (page) => !page.includes('/admin') && !page.includes('/draft'),
-      changefreq: 'weekly',
-      priority: 0.7,
-      lastmod: new Date(),
-    })
+    mdx()
   ]
 });
