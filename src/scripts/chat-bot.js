69489c2740e7610658aa58c61936bc50a264423b
Shubhanshu Pandey <webdevshubhanshu@gmail.com>
2026-07-08 17:52:20 +0530

    adding the discord chatbot

--- src/scripts/chat-bot.js
+++ src/scripts/chat-bot.js
@@ -1,5 +1,6 @@
 window.$crisp = [];
-window.CRISP_WEBSITE_ID = "4abd2d6b-9b40-4da0-9159-556eeb546157";
+// window.CRISP_WEBSITE_ID = "4abd2d6b-9b40-4da0-9159-556eeb546157";
+window.CRISP_WEBSITE_ID = "e8003f14-3892-48ce-99d6-0e1235596030";  //added by Shubhanshu for the testing
 (function () {
     const d = document;
     const s = d.createElement("script");
